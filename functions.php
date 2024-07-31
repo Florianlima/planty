a4601775c18f9b7008bebd4dfc25d671f4130a18
florian <florianlima51@gmail.com>
2024-07-31 15:56:14 +0200

    modif hook

--- functions.php
+++ functions.php
@@ -10,17 +10,14 @@ function theme_enqueue_styles()
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
 }
 
-register_nav_menus(array(
-    'Nous rencontrer' => __('Nous rencontrer')
-));
 
 
-add_filter('wp_nav_menu_items', 'add_admin_link', 10, 2);
+add_filter('wp_nav_menu_items', 'add_admin_link', 10, 3);
 
 function add_admin_link($items, $args)
 {
     if (is_user_logged_in() && $args->theme_location == 'Nous rencontrer') {
         $items .= '<li class="admin"><a href="' . get_admin_url() . '">Admin</a></li>';
     }
-    return $items;
+    return $items; return $args ;
 }
